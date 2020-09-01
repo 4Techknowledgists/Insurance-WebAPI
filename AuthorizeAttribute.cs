c8568c85d26b2e6bf5f927b389040cc444704670
sreenivasapbang <66455934+sreenivasapbang@users.noreply.github.com>
2020-09-01 23:30:30 +0530

    Initial commit
    BAL structure with API for reliance

---
+++ AuthorizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiTokenAuthentication
+{
+    public class AuthorizeAttribute : System.Web.Http.AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContexts)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                base.HandleUnauthorizedRequest(actionContexts);
+            }
+            else
+            {
+                actionContexts.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
